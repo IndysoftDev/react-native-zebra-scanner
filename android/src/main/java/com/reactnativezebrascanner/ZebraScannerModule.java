80808010e2ca79e908ba84f2077a99c7dd94f380
“Sam <sam.joseph@indysoft.com>
2021-09-09 11:39:22 -0400

    add zebra functionality

--- android/src/main/java/com/reactnativezebrascanner/ZebraScannerModule.java
+++ android/src/main/java/com/reactnativezebrascanner/ZebraScannerModule.java
@@ -1,40 +1,276 @@
 package com.reactnativezebrascanner;
 
-import androidx.annotation.NonNull;
+import android.util.Log;
+import android.view.View;
 
+import com.facebook.react.bridge.WritableArray;
+import com.facebook.react.bridge.Arguments;
+import com.facebook.react.bridge.LifecycleEventListener;
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.modules.core.DeviceEventManagerModule;
 import com.facebook.react.module.annotations.ReactModule;
 
-import com.zebra.scannercontrol.SDKHandler;
+import com.zebra.scannercontrol.*;
+
+import java.util.ArrayList;
+
+import static com.zebra.scannercontrol.DCSSDKDefs.DCSSDK_MODE.DCSSDK_OPMODE_BT_NORMAL;
 
 @ReactModule(name = ZebraScannerModule.NAME)
-public class ZebraScannerModule extends ReactContextBaseJavaModule {
+public class ZebraScannerModule extends ReactContextBaseJavaModule
+        implements LifecycleEventListener, IDcsSdkApiDelegate {
+
     public static final String NAME = "ZebraScanner";
-    private static final String TAG = "ReactNative";
+    private final String LOG = "[ZEBRA Barcode]";
+    private final String BARCODE = "BARCODE";
     private final ReactApplicationContext reactContext;
-    private SDKHandler sdkHandler; // Zebra SDK
+    private static SDKHandler sdkHandler = null;
+    private static DCSScannerInfo scanner = null;
+    // Barcode library register listener.
+    private static int notifications_mask = 0;
 
     public ZebraScannerModule(ReactApplicationContext reactContext) {
         super(reactContext);
         this.reactContext = reactContext;
-        this.sdkHandler = new SDKHandler(reactContext);
+        this.reactContext.addLifecycleEventListener(this);
     }
 
     @Override
-    @NonNull
+    public void onHostResume() {
+
+    }
+
+    @Override
+    public void onHostPause() {
+
+    }
+
+    @Override
+    public void onHostDestroy() {
+        doDisconnect();
+
+        dispose();
+    }
+
+    @Override
     public String getName() {
         return NAME;
     }
 
-    // Example method
-    // See https://reactnative.dev/docs/native-modules-android
-    @ReactMethod
-    public void multiply(int a, int b, Promise promise) {
-        promise.resolve(a * b);
+    private void sendEvent(String eventName, WritableMap params) {
+        this.reactContext.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class).emit(eventName, params);
     }
 
-    public static native int nativeMultiply(int a, int b);
+    private void sendEvent(String eventName, String msg) {
+        this.reactContext.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class).emit(eventName, msg);
+    }
+
+    @ReactMethod
+    public void connect(String name, Promise promise) {
+        try {
+            if (sdkHandler == null) {
+                init();
+            }
+            ArrayList<DCSScannerInfo> list = new ArrayList<>();
+            sdkHandler.dcssdkGetAvailableScannersList(list);
+
+            for (DCSScannerInfo item : list) {
+                if (item.getScannerName().equals(name)) {
+                    scanner = item;
+                }
+            }
+
+            doConnect();
+            promise.resolve(true);
+        } catch (Exception ex) {
+            promise.reject(ex);
+        }
+    }
+
+    @ReactMethod
+    public void getScanners(Promise promise) {
+        try {
+            if (sdkHandler == null) {
+                init();
+            }
+
+            ArrayList<DCSScannerInfo> devices = new ArrayList<>();
+            sdkHandler.dcssdkGetAvailableScannersList(devices);
+            final WritableArray payload = new WritableArray();
+            for (DCSScannerInfo device : devices) {
+                payload.pushMap(toScannerDeviceMap(device));
+            }
+
+            promise.resolve(payload);
+        } catch (Exception ex) {
+            promise.reject(ex);
+        }
+    }
+
+    private WritableMap toScannerDeviceMap(final DCSScannerInfo scanner) {
+        final WritableMap device = Arguments.createMap();
+        device.putString("name", scanner.getScannerName());
+        device.putString("address", scanner.getScannerHWSerialNumber());
+        return device;
+    }
+
+    @ReactMethod
+    public void reconnect() {
+        doConnect();
+    }
+
+    @ReactMethod
+    public void disconnect() {
+        doDisconnect();
+    }
+
+    @ReactMethod
+    public void softTriggerStart() {
+        barcodePullTrigger();
+    }
+
+    @ReactMethod
+    public void softTriggerStop() {
+        barcodeReleaseTrigger();
+    }
+
+    @Override
+    public void dcssdkEventScannerAppeared(DCSScannerInfo dcsScannerInfo) {
+
+    }
+
+    @Override
+    public void dcssdkEventScannerDisappeared(int i) {
+
+    }
+
+    @Override
+    public void dcssdkEventCommunicationSessionEstablished(DCSScannerInfo dcsScannerInfo) {
+
+    }
+
+    @Override
+    public void dcssdkEventCommunicationSessionTerminated(int i) {
+
+    }
+
+    @Override
+    public void dcssdkEventBarcode(byte[] bytes, int i, int i1) {
+        sendEvent(BARCODE, new String(bytes));
+    }
+
+    @Override
+    public void dcssdkEventImage(byte[] bytes, int i) {
+
+    }
+
+    @Override
+    public void dcssdkEventVideo(byte[] bytes, int i) {
+
+    }
+
+    @Override
+    public void dcssdkEventBinaryData(byte[] bytes, int i) {
+
+    }
+
+    @Override
+    public void dcssdkEventFirmwareUpdate(FirmwareUpdateEvent firmwareUpdateEvent) {
+
+    }
+
+    @Override
+    public void dcssdkEventAuxScannerAppeared(DCSScannerInfo dcsScannerInfo, DCSScannerInfo dcsScannerInfo1) {
+
+    }
+
+    private void init() {
+        if (sdkHandler == null) {
+            sdkHandler = new SDKHandler(this.reactContext);
+            sdkHandler.dcssdkSetDelegate(this);
+
+            sdkHandler.dcssdkSetOperationalMode(DCSSDKDefs.DCSSDK_MODE.DCSSDK_OPMODE_BT_NORMAL);
+
+            sdkHandler.dcssdkEnableAvailableScannersDetection(true);
+
+            notifications_mask |= (DCSSDKDefs.DCSSDK_EVENT.DCSSDK_EVENT_SCANNER_APPEARANCE.value
+                    | DCSSDKDefs.DCSSDK_EVENT.DCSSDK_EVENT_SCANNER_DISAPPEARANCE.value);
+            notifications_mask |= (DCSSDKDefs.DCSSDK_EVENT.DCSSDK_EVENT_SESSION_ESTABLISHMENT.value
+                    | DCSSDKDefs.DCSSDK_EVENT.DCSSDK_EVENT_SESSION_TERMINATION.value);
+            notifications_mask |= (DCSSDKDefs.DCSSDK_EVENT.DCSSDK_EVENT_BARCODE.value);
+        }
+    }
+
+    private void dispose() {
+        Log.d(LOG, "dispose");
+
+        if (sdkHandler != null) {
+            sdkHandler.dcssdkClose();
+
+            scanner = null;
+            sdkHandler = null;
+            notifications_mask = 0;
+        }
+    }
+
+    private void doConnect() {
+        if (scanner != null && scanner.isActive()) {
+            doDisconnect();
+        }
+
+        if (sdkHandler != null && scanner != null && !scanner.isActive()) {
+            DCSSDKDefs.DCSSDK_RESULT result = DCSSDKDefs.DCSSDK_RESULT.DCSSDK_RESULT_FAILURE;
+            sdkHandler.dcssdkSubsribeForEvents(notifications_mask);
+            result = sdkHandler.dcssdkEstablishCommunicationSession(scanner.getScannerID());
+
+            Log.d(LOG, result.name());
+        }
+    }
+
+    private void doDisconnect() {
+        if (sdkHandler != null && scanner != null && scanner.isActive()) {
+            sdkHandler.dcssdkUnsubsribeForEvents(notifications_mask);
+            DCSSDKDefs.DCSSDK_RESULT result = DCSSDKDefs.DCSSDK_RESULT.DCSSDK_RESULT_FAILURE;
+            result = sdkHandler.dcssdkTerminateCommunicationSession(scanner.getScannerID());
+
+            Log.d(LOG, result.name());
+        }
+    }
+
+    private void barcodePullTrigger() {
+        if (scanner != null) {
+            StringBuilder outXML = null;
+            String in_xml = "<inArgs><scannerID>" + scanner.getScannerID() + "</scannerID></inArgs>";
+            DCSSDKDefs.DCSSDK_COMMAND_OPCODE opcode = DCSSDKDefs.DCSSDK_COMMAND_OPCODE.DCSSDK_DEVICE_PULL_TRIGGER;
+            executeCommand(opcode, in_xml, outXML, scanner.getScannerID());
+        }
+    }
+
+    private void barcodeReleaseTrigger() {
+        if (scanner != null) {
+            StringBuilder outXML = null;
+            String in_xml = "<inArgs><scannerID>" + scanner.getScannerID() + "</scannerID></inArgs>";
+            DCSSDKDefs.DCSSDK_COMMAND_OPCODE opcode = DCSSDKDefs.DCSSDK_COMMAND_OPCODE.DCSSDK_DEVICE_RELEASE_TRIGGER;
+            executeCommand(opcode, in_xml, outXML, scanner.getScannerID());
+        }
+    }
+
+    private DCSSDKDefs.DCSSDK_RESULT executeCommand(DCSSDKDefs.DCSSDK_COMMAND_OPCODE opCode, String inXML,
+            StringBuilder outXML, int scannerID) {
+        DCSSDKDefs.DCSSDK_RESULT result = DCSSDKDefs.DCSSDK_RESULT.DCSSDK_RESULT_FAILURE;
+
+        if (sdkHandler != null) {
+            if (outXML == null) {
+                outXML = new StringBuilder();
+            }
+
+            result = sdkHandler.dcssdkExecuteCommandOpCodeInXMLForScanner(opCode, inXML, outXML, scannerID);
+        }
+        return result;
+    }
 }
