9f5c8ccc4247f1f3866eb3ccc051e3b7442654e0
“Sam <sam.joseph@indysoft.com>
2021-09-09 14:44:57 -0400

    add barcode listener

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,31 @@
-import { NativeModules } from 'react-native';
+import { NativeModules, NativeEventEmitter } from 'react-native';
 
 const { ZebraScanner } = NativeModules;
 
+const evtEmitter = new NativeEventEmitter(ZebraScanner);
+
 type getScanners = () => Promise<any>;
 
-type connect = (scannerName: string) => Promise<any>;
+type initReader = (scannerName: string) => Promise<any>;
+
+type addListener = (cb: (args: any[]) => void) => void;
+
+type removeListener = (cb: (args: any[]) => void) => void;
 
 const getScanners: getScanners = () => ZebraScanner.getScanners();
 
-const connect: connect = (scannerName) => ZebraScanner.connect(scannerName);
+const initReader: initReader = (scannerName) =>
+  ZebraScanner.connect(scannerName);
+
+const addBarcodeListener: addListener = (listener) =>
+  evtEmitter.addListener('BARCODE', listener);
+
+const removeBarcodeListener: removeListener = (listener) =>
+  evtEmitter.removeListener('BARCODE', listener);
 
 export default {
   getScanners,
-  connect,
+  initReader,
+  addBarcodeListener,
+  removeBarcodeListener,
 };
